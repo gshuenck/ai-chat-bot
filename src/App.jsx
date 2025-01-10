4a92f6c2132a940728b0d077fb36314c3287817e
Gilmar Shuenck <gilmar_shuenck@hotmail.com>
2025-01-10 15:21:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ChatBotStart from "./Components/ChatBotStart";
+import ChatBotApp from "./Components/ChatBotApp";
+
+export const App = () => {
+  return (
+    <div className='container'>
+      {/* <ChatBotStart/> */}
+      <ChatBotApp />
+    </div>
+  )
+}
+export default App
